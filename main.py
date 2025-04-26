04704142ac9d5506e6a178c7d2096118de26d831
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-26 21:58:46 +0300

    Implemented serializer

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    return CarSerializer(car).data
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    return JSONRenderer().render(json)
