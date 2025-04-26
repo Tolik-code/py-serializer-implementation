fdb3326491bfcf6ae837193e55dad56b10f78155
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-26 22:09:53 +0300

    fixed deserialize_car_object

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
+from io import BytesIO
 
 from car.models import Car
 from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    return CarSerializer(car).data
+    return JSONRenderer().render(CarSerializer(car).data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    return JSONRenderer().render(json)
+    stream = BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+
+    if serializer.is_valid():
+        return serializer.save()
+    else:
+        raise ValueError(f"Invalid data: {serializer.errors}")
